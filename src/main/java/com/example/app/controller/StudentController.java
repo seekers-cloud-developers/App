d1a213e73cd7e2f793bd7e24d5111c3e76b17533
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-12-26 19:43:45 +0530

    init

---
+++ src/main/java/com/example/app/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.app.controller;
+
+
+import com.example.app.db.DataBase;
+import com.example.app.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("api/v1/student")
+public class StudentController {
+
+    @GetMapping("/all")
+    public ArrayList<Student> getAllStudents(){
+        return DataBase.studentsDatable;
+    }
+}
