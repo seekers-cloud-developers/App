f9c176869e175bbbf36b873b11e240b670f0d199
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-12-26 20:00:51 +0530

    iit

--- src/main/java/com/example/app/controller/StudentController.java
+++ src/main/java/com/example/app/controller/StudentController.java
@@ -4,6 +4,7 @@ package com.example.app.controller;
 import com.example.app.db.DataBase;
 import com.example.app.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentController {
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id){
+        return DataBase.studentsDatable.get(id);
+    }
 
     @GetMapping("/all")
     public ArrayList<Student> getAllStudents(){
